beb73948e2fb4698cb2e447465baeb2cbf8b6d3b
vlurgio <13vlurgio@gmail.com>
2018-04-16 20:40:50 -0400

    update

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+      persons: [
+          { name: 'Max', age: 28 },
+          { name: 'Manu', age: 29 },
+          { name: 'Stephanie', age:26 }
+      ],
+      otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+      this.setState({persons: [{ name: 'Whore Breath', age: 28 },
+                              { name: 'Manu', age: 29 },
+                              { name: 'Stephanie', age:29 }]})
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+       <h1>Hi, I'm a react app.</h1>
+          <button onClick={this.switchNameHandler}>Switch Name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies include fucking hookers!</Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
+
   }
 }
 
